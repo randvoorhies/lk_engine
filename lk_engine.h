303a229151190cef413d3dae4e5b0a2413abf6c1
Randolph Voorhies <rand.voorhies@gmail.com>
2012-08-12 16:09:19 -0700

    Tracking and timing now working

--- lk_engine.h
+++ lk_engine.h
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/core/core_c.h>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/tracking.hpp>
 #include <boost/shared_ptr.hpp>
@@ -11,7 +12,9 @@ namespace rcv
     public:
       LKEngine();
       void updateImage(cv::Mat const image);
-      void trackPoints(std::vector<cv::Point2f> const & prev_points, CvSize win_size);
+      void track(std::vector<cv::Point2f> const & prev_points,
+          std::vector<cv::Point2f> & curr_points, std::vector<char> & status, std::vector<float> & error,
+          CvSize win_size);
 
     private:
       void allocate();
@@ -30,13 +33,29 @@ namespace rcv
 rcv::LKEngine::LKEngine() :
   level_(1),
   flags_(0)
-{
-}
+{ }
 
 // ######################################################################
 void rcv::LKEngine::updateImage(cv::Mat const image)
 {
-  if(!prev_pyramid_ || !curr_pyramid_) allocate();
+  prev_image_ = curr_image_;
+
+  if(image.type() == CV_8UC1)
+    curr_image_ = image;
+  else if(image.type() == CV_8UC3)
+    cv::cvtColor(image, curr_image_, CV_RGB2GRAY);
+
+  if(!prev_pyramid_ || !curr_pyramid_)
+  {
+    allocate();
+    curr_image_.copyTo(prev_image_);
+  }
+
+  std::swap(prev_pyramid_, curr_pyramid_);
+  if(flags_ & CV_LKFLOW_PYR_B_READY)
+    flags_ |= CV_LKFLOW_PYR_A_READY;
+
+  flags_ &= ~CV_LKFLOW_PYR_B_READY;
 }
 
 // ######################################################################
@@ -47,11 +66,13 @@ void rcv::LKEngine::allocate()
 }
 
 // ######################################################################
-void rcv::LKEngine::trackPoints(std::vector<cv::Point2f> const & prev_points, CvSize win_size)
+void rcv::LKEngine::track(std::vector<cv::Point2f> const & prev_points,
+    std::vector<cv::Point2f> & curr_points, std::vector<char> & status, std::vector<float> & error,
+    CvSize win_size)
 {
-  std::vector<cv::Point2f> curr_points(prev_points.size());
-  std::vector<char> status(prev_points.size());
-  std::vector<float> error(prev_points.size());
+  curr_points.resize(prev_points.size());
+  status.resize(prev_points.size());
+  error.resize(prev_points.size());
 
   CvTermCriteria term_criteria
     = cvTermCriteria( CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, .3 );
@@ -60,10 +81,12 @@ void rcv::LKEngine::trackPoints(std::vector<cv::Point2f> const & prev_points, Cv
   IplImage curr_ipl = curr_image_;
 
   CvPoint2D32f const * prev_points_arr = reinterpret_cast<CvPoint2D32f const *>(&prev_points[0]);
-  CvPoint2D32f * curr_points_arr = reinterpret_cast<CvPoint2D32f*>(&curr_points[0]);
+  CvPoint2D32f * curr_points_arr       = reinterpret_cast<CvPoint2D32f*>(&curr_points[0]);
 
   cvCalcOpticalFlowPyrLK(&prev_ipl, &curr_ipl, prev_pyramid_.get(), curr_pyramid_.get(),
       prev_points_arr, curr_points_arr, prev_points.size(),
       win_size, level_, &status[0], &error[0], term_criteria, flags_);
 
+  flags_ |= CV_LKFLOW_PYR_A_READY;
+  flags_ |= CV_LKFLOW_PYR_B_READY;
 }
--- test_lk_engine.cpp
+++ test_lk_engine.cpp
@@ -1,5 +1,6 @@
 #include "lk_engine.h"
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char** argv)
 {
@@ -8,13 +9,40 @@ int main(int argc, char** argv)
   cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
   rcv::LKEngine lk;
+    
+  std::vector<cv::Point2f> prev_points;
 
   while(true)
   {
     cv::Mat image;
     cap >> image;
 
+    cv::cvtColor(image, image, CV_RGB2GRAY);
+
+    auto features_start = std::chrono::monotonic_clock::now();
+    cv::goodFeaturesToTrack(image, prev_points, 50, .01, 15);
+    std::chrono::microseconds features_time =
+      std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::monotonic_clock::now() - features_start);
+    std::cout << "Features took " << features_time.count() / 1000.0 << "ms" << std::endl;
+
+    auto update_start = std::chrono::monotonic_clock::now();
     lk.updateImage(image);
+    std::chrono::microseconds update_time =
+      std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::monotonic_clock::now() - update_start);
+    std::cout << "Update took " << update_time.count() / 1000.0 << "ms" << std::endl;
+
+
+
+    std::vector<cv::Point2f> curr_points;
+    std::vector<char> status;
+    std::vector<float> error;
+    CvSize win_size = cvSize(15,15);
+
+    auto track_start = std::chrono::monotonic_clock::now();
+    lk.track(prev_points, curr_points, status, error, win_size);
+    std::chrono::microseconds track_time =
+      std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::monotonic_clock::now() - track_start);
+    std::cout << "Track took " << track_time.count() / 1000.0 << "ms" << std::endl;
 
     cv::imshow("image", image);
 
